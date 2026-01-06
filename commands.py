145a0c340320a791f7777d245c54b753362e1a8f
Andrei Dina <andreidina04@gmail.com>
2026-01-06 10:27:57 +0200

    Add folder creation and system info functions

--- commands.py
+++ commands.py
@@ -1,6 +1,9 @@
 import os
 import webbrowser
 import subprocess
+import datetime
+from pathlib import Path
+import platform
 
 def open_links(user_text):
     websites = {"google": "https://www.google.com",
@@ -21,3 +24,40 @@ def open_links(user_text):
         webbrowser.open("https://www.youtube.com")
         return f"Opened Youtube for you! Enjoy watching!"
     return "I couldn't figure out which website to open."
+
+
+def create_folder(user_text):
+    folder_keywords = ["create folder", "folder", "directory", "mkdir"]
+    extracted_text = user_text.lower()
+    for word in folder_keywords:
+        extracted_text = extracted_text.replace(word, "")
+    folder_name = extracted_text.strip()
+    if not folder_name:
+        return "Please specify a name. Example: 'folder MyProject'"
+    try:
+        desktop_path = Path.home() / "Desktop" / folder_name
+        desktop_path.mkdir(parents=True, exist_ok=True)
+        return f"Created the folder '{folder_name}' on Desktop for you!"
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+def greetings(user_text):
+        return "Glad to help! Everytime when you need me I'm here."
+
+def close_app(user_text):
+    return "Bye! The application is closing..."
+
+def show_time(user_text):
+    current_time = datetime.datetime.now()
+    return current_time.strftime("Date: %d.%m.%Y | Hour: %H:%M:%S")
+
+def computer_info(user_text):
+    my_system = platform.uname()
+    return (f"\nSystem Information\n"
+            "---------------------\n"
+            f"OS : {my_system.system}\n"
+            f"Node Name : {my_system.node}\n"
+            f"Release : {my_system.release}\n"
+            f"Version : {my_system.version}\n"
+            f"Machine : {my_system.machine}\n"
+            f"Processor : {my_system.processor}")
