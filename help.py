2fb57b787b8c794a44b38e74c750d5df9a240569
Andrei Dina <andreidina04@gmail.com>
2026-01-08 23:03:00 +0200

    Add 'create file' command to help text

--- help.py
+++ help.py
@@ -2,6 +2,7 @@ def show_help(user_text):
     return ("\nAvailable Commands\n"
             "---------------------\n"
             "· create folder folder_name\n"
+            "· create file file_name\n"
             "· open youtube / google / github\n"
             "· quit\n"
             "· show time\n"
