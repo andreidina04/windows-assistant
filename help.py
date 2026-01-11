3f0bc2ac565eecac32ee244a4276944fe59a394f
Andrei Dina <andreidina04@gmail.com>
2026-01-11 17:04:22 +0200

    Add 'open app_name' command to help text

--- help.py
+++ help.py
@@ -4,6 +4,7 @@ def show_help(user_text):
             "· create folder folder_name\n"
             "· create file file_name\n"
             "· open youtube / google / github\n"
+            "· open app_name\n"
             "· quit\n"
             "· show time\n"
             "· system info\n"
